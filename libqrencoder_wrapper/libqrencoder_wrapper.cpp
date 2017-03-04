233df964215a878ac1a9b44649c13e15d5c55531
pwrobel <wrobel1989@gmail.com>
2017-03-04 11:55:30 +0100

    Convert to grayscale jpeg for faster processing.

--- libqrencoder_wrapper/libqrencoder_wrapper.cpp
+++ libqrencoder_wrapper/libqrencoder_wrapper.cpp
@@ -2,6 +2,21 @@
 #include "libqrencoder_wrapper.h"
 
 
+#include <sys/time.h>
+
+
+double currmili(){
+    struct timeval start;
+    double mtime, seconds, useconds;
+    gettimeofday(&start, NULL);
+    seconds  = start.tv_sec;
+    useconds = start.tv_usec;
+    mtime = ((seconds) * 1000.0 + useconds/1000.0) + 0.5;
+    return mtime;
+}
+
+
+
 JSAMPLE  image_buffer[] = {0x00,0x00,0x00, 0xff,0xff,0xff, 0xff,0xff,0xff, 0x00,0,0};	/* Points to large array of R,G,B-order data */
 int image_height=2;	/* Number of rows in image */
 int image_width=2;		/* Number of columns in image */
@@ -19,15 +34,15 @@ size_t compressImage(char* buffer, size_t width, size_t height, unsigned char** 
 
   JSAMPROW row_pointer[1];  //Pointer to JSAMPLE row[s]
 
-  int row_stride = width * 3; //Physical row width in image buffer
+  int row_stride = width * 1; //Physical row width in image buffer
   cinfo.err      = jpeg_std_error(&jerr);
 
   jpeg_create_compress(&cinfo);
 
   cinfo.image_width        = width;
   cinfo.image_height       = height;
-  cinfo.input_components   = 3;
-  cinfo.in_color_space     = JCS_RGB;
+  cinfo.input_components   = 1;
+  cinfo.in_color_space     = JCS_GRAYSCALE;
   size_t outlen            = 0;
 
   jpeg_mem_dest(&cinfo, &outp, &outlen);
@@ -62,6 +77,7 @@ void finish_libqrencoder(){
 void generate_image_data(const unsigned char* input_data, int input_length, char** out_image_data, int *out_image_data_size,
                          int *max_target_width){
 
+    double t = currmili();
   QRcode *generatedQR = NULL;
   generatedQR = QRcode_encodeData(input_length, input_data, 1, QR_ECLEVEL_M);
   unsigned char* QR_pixeldata = NULL;
@@ -82,37 +98,22 @@ void generate_image_data(const unsigned char* input_data, int input_length, char
   int target_width = *max_target_width;
 
 
-  //*out_image_data_size = target_width * target_width * 3; // RGB buffer size
-  //*out_image_data = new char[*out_image_data_size]; //RGB buffer as an input for jpeg creation
-
-
-  char* out_target_rgb_image = new char[target_width*target_width*3];
-  memset(out_target_rgb_image, 0, target_width*target_width*3);
+  char* out_target_rgb_image = new char[target_width*target_width];
+  memset(out_target_rgb_image, 0, target_width*target_width);
 
   for (int i = 0; i<target_width; i++) {
       for (int j = 0; j<target_width; j++) {
           char val = ((QR_pixeldata[(int)(i*(((float)generatedQR->width)/target_width))*generatedQR->width+(int)(j*((((float)generatedQR->width)/target_width)))] & 1) ? 0 : 0xff);
-          out_target_rgb_image[3*(i*target_width+j)+0] = val;
-          out_target_rgb_image[3*(i*target_width+j)+1] = val;
-          out_target_rgb_image[3*(i*target_width+j)+2] = val;
-          if(i==0)printf(" %02x", val);
+          out_target_rgb_image[(i*target_width+j)] = val;
       }
   }
 
-  /*
-  printf("\n");
-  for (int i = 0; i<generatedQR->width; i++) {
-      for (int j = 0; j<generatedQR->width; j++) {
-          printf("%02x ", QR_pixeldata[i*generatedQR->width+j]);
-      }
-      printf("\n");
-  }
-  //memcpy(*out_image_data, image_buffer, 12);
-  */
   unsigned char* out_jpeg_buff; //pointer to the generated data in the static buffer
   int jpegsize = compressImage(out_target_rgb_image, target_width, target_width, &out_jpeg_buff, 100);
 
   delete []out_target_rgb_image;
+
+  printf("MS genQR %f\n", currmili() - t);
 
   FILE *my_file = fopen("dump11.jpg", "wb");
   fwrite(out_jpeg_buff, jpegsize , 1, my_file);
--- libqrencoder_wrapper/libqrencoder_wrapper_test.cpp
+++ libqrencoder_wrapper/libqrencoder_wrapper_test.cpp
@@ -8,11 +8,11 @@
 
 
 int main(){
-  const unsigned char* data = (const unsigned char*)"abcQQdef";
+  const unsigned char* data = (const unsigned char*)"In computing, plain text is the data (e.g. file contents) that represent only characters of readable material but not its";
   int len = strlen((const char*)data);
   int out_len;
   char** out_data;
-  int max_target_width = 200;
+  int max_target_width = 600;
   generate_image_data(data, len, out_data, &out_len, &max_target_width);
   return 1;
 }
