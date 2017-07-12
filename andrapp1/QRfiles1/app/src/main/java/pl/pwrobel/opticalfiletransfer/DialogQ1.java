72b68e06e0940084aeb2d4d10cab6f8c27ce53f6
Pawel Wrobel <wrobel1989@gmail.com>
2017-07-12 23:01:21 +0200

    Improve text formatting.

--- andrapp1/QRfiles1/app/src/main/java/pl/pwrobel/opticalfiletransfer/DialogQ1.java
+++ andrapp1/QRfiles1/app/src/main/java/pl/pwrobel/opticalfiletransfer/DialogQ1.java
@@ -4,13 +4,54 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
+
+import java.util.Locale;
 
 /**
  * Created by pwrobel on 05.07.17.
  */
 
 public class DialogQ1 extends DialogFragment {
+
+    /**
+     * Makes a substring of a string bold.
+     * @param text          Full text
+     * @param textToBold    Text you want to make bold
+     * @return              String with bold substring
+     */
+
+    public static SpannableStringBuilder makeSectionOfTextBold(String text, String textToBold){
+
+        SpannableStringBuilder builder=new SpannableStringBuilder();
+
+        if(textToBold.length() > 0 && !textToBold.trim().equals("")){
+
+            //for counting start/end indexes
+            String testText = text.toLowerCase(Locale.US);
+            String testTextToBold = textToBold.toLowerCase(Locale.US);
+            int startingIndex = testText.indexOf(testTextToBold);
+            int endingIndex = startingIndex + testTextToBold.length();
+            //for counting start/end indexes
+
+            if(startingIndex < 0 || endingIndex <0){
+                return builder.append(text);
+            }
+            else if(startingIndex >= 0 && endingIndex >=0){
+
+                builder.append(text);
+                builder.setSpan(new StyleSpan(Typeface.BOLD), startingIndex, endingIndex, 0);
+            }
+        }else{
+            return builder.append(text);
+        }
+
+        return builder;
+    }
+
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
